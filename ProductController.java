f5769f2acf45cf8cff8a684a29e36c37888f2799
dipakmaske <73223364+dipakmaske@users.noreply.github.com>
2020-10-25 20:01:48 +0530

    productController

---
+++ ProductController.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ProductController {
+
+		private int productid;
+		private float price;
+		private String color;
+		private String Pname;
+		Scanner sc= new Scanner(System.in);
+		
+		public void ProductCreate()
+		{
+			System.out.println("Enter Product ID :");
+			productid =sc.nextInt();
+			System.out.println("Enter Product Price :");
+			price =sc.nextFloat();
+			System.out.println("Enter Product color :");
+			color =sc.next();
+			System.out.println("Enter Product Name :");
+			Pname =sc.next();
+		}
+		
+		public void UpdateProduct(int productid , float price , String color , String Pname)
+		{
+			this.productid=productid;
+			this.price=price;
+			this.color=color;
+			this.Pname=Pname;
+			
+		}
+		
+		
+			
+
+	}
+
